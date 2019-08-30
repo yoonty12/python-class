4b8675315c9d3a1a2bb875ed8abc09bc17c0e148
yoonty12 <54702568+yoonty12@users.noreply.github.com>
2019-08-30 16:40:06 +0900

    190830 python class

---
+++ test1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 13:51:12 2019
+
+@author: Administrator
+"""
+
+#&&
+a=1
+print (a)
---
+++ test3.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:43:18 2019
+
+@author: Administrator
+"""
+
+#%%
+# '안녕'을 출력해봅시다.
+print('안녕')
+
+#%%
+print('안녕' + '하세요')
+
+#%%
+print('안녕', '하세요')
+
+#%%
+print('1+4')
+
+#%%
+print(1+4)
+
+#%%
+print(1/4)
+
+#%%
+print ('5' / 3)
+
+#%%
+print('5' + '3')
+
+#%%
+print('5' * 3)
+
+#%%
+a='5'
+print(a)
+b='김치'
+milk='물'
+_car=153
+
+a=33
+print(a)
+
+# class, if, while, def
+
+#%%
+a1=1
+print(a1)
+
+
+
